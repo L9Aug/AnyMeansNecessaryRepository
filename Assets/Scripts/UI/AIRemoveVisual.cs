1896e8e7eeaab0a27329bfcd19a5f0cf3fa4d6b2
up723163 <up723163@S7DEL125A-09>
2017-03-09 14:38:23 +0000

    commit

--- Assets/Scripts/UI/AIRemoveVisual.cs
+++ Assets/Scripts/UI/AIRemoveVisual.cs
@@ -1,31 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AIRemoveVisual : MonoBehaviour {
-
-   
-
+public class AIRemoveVisual : MonoBehaviour
+{
     GameObject AI;
-   
-	// Use this for initialization
-	void Start () {
-        AI = transform.parent.gameObject;
-  
-        
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
+    // Use this for initialization
+    void Start()
+    {
+        AI = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (AI.GetComponent<Base_Enemy>()._state == Base_Enemy.State.Dead)
         {
             Destroy(gameObject);
         }
-	}
+    }
 
     public static void removeMiniMapVisual()
     {
-        
+
     }
+
 }
