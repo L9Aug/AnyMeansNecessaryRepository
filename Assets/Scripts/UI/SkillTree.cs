7bc4de89c16f67be0accae6036b6f20eb7dc783c
up690813 <up690813@myport.ac.uk>
2017-04-14 17:52:48 +0100

    First bug was unable to recreate, Leveling bug has been fixed.

--- Assets/Scripts/UI/SkillTree.cs
+++ Assets/Scripts/UI/SkillTree.cs
@@ -7,61 +7,56 @@ public class SkillTree : MonoBehaviour
     public static int skillPoints = 0;
 
     public bool intialSkill;
-    public GameObject [] previousSkill;
-    public static Text SkillPointCounter;
+    public GameObject[] previousSkill;
     public SkillsController.Skills Skill;
 
     Button currentSkill;
     bool purchased;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Awake()
+    {
         currentSkill = this.gameObject.GetComponent<Button>();
         skillCheck();
-        SkillPointCounter = GameObject.Find("Number of Points").GetComponent<Text>();
-        SkillPointCounter.text = skillPoints.ToString();
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-        skillCheck();       
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        skillCheck();
+    }
 
     public void purchaseSkill()
     {
         if (skillPoints > 0 && purchased != true)
-        {           
+        {
             purchased = true;
             currentSkill.image.color = Color.grey;
             skillPoints--;
-            SkillPointCounter.text = skillPoints.ToString();
+            //SkillPointCounter.text = skillPoints.ToString();
             SkillsController.SC.AddNewSkill(Skill);
         }
-        
     }
 
     void skillCheck()
     {
-            for (int i = 0; i < previousSkill.Length; i++)
+        for (int i = 0; i < previousSkill.Length; i++)
+        {
+            if (intialSkill == false && previousSkill[i].GetComponent<SkillTree>().purchased == false)
             {
-                if (intialSkill == false && previousSkill[i].GetComponent<SkillTree>().purchased == false)
-                {
-                    currentSkill.interactable = false;
-                    //Debug.Log("Called");
-                }
-                if (previousSkill[i].GetComponent<SkillTree>().purchased == true)
-                {
-                    currentSkill.interactable = true;
-                   // Debug.Log("Called.2");
-                }
+                currentSkill.interactable = false;
+                //Debug.Log("Called");
             }
-        
+            if (previousSkill[i].GetComponent<SkillTree>().purchased == true)
+            {
+                currentSkill.interactable = true;
+                // Debug.Log("Called.2");
+            }
+        }
     }
 
     public static void updateSkillPoints()
     {
-        SkillPointCounter.text = skillPoints.ToString();
+        //SkillPointCounter.text = skillPoints.ToString();
     }
-
 }
